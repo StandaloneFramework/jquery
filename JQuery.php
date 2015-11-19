598064c39ed92a5d0f14d90e1671bc8ddd984915
bllem <art.kaplenko@gmail.com>
2015-11-19 18:25:34 +0200

    init

---
+++ JQuery.php
@@ -0,0 +1,29 @@
+<?php
+namespace standaloneFramework\jquery;
+
+use app\application\classes\base\BaseBlock;
+
+/**
+ * Class JQuery
+ *
+ * jquery block for connection into framework
+ * required jquery from components\jquery
+ *
+ * @package standaloneFramework\jquery
+ * @author Artem Kaplenko <art.kaplenko@gmail.com>
+ * @version 1.0.0
+ */
+class Jquery extends BaseBlock{
+    /**
+     * block name
+     */
+    const NAME = 'jquery';
+
+    /**
+     * Jquery file
+     * @var array
+     */
+    public static $jsAsset = [
+        'jquery.min.js'
+    ];
+}
